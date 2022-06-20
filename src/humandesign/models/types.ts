6850c6c18b2cde0294fccfe725f7a57ba999e61a
Tyler Gannon <tgannon@gmail.com>
2022-06-20 12:24:47 -0600

    Update: Gate numbers are type-checked strings.

--- src/humandesign/models/types.ts
+++ src/humandesign/models/types.ts
@@ -182,11 +182,77 @@ export interface YinYang {
     other(): YinYang
 }
 
+export type GateNum =
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12'
+    | '13'
+    | '14'
+    | '15'
+    | '16'
+    | '17'
+    | '18'
+    | '19'
+    | '20'
+    | '21'
+    | '22'
+    | '23'
+    | '24'
+    | '25'
+    | '26'
+    | '27'
+    | '28'
+    | '29'
+    | '30'
+    | '31'
+    | '32'
+    | '33'
+    | '34'
+    | '35'
+    | '36'
+    | '37'
+    | '38'
+    | '39'
+    | '40'
+    | '41'
+    | '42'
+    | '43'
+    | '44'
+    | '45'
+    | '46'
+    | '47'
+    | '48'
+    | '49'
+    | '50'
+    | '51'
+    | '52'
+    | '53'
+    | '54'
+    | '55'
+    | '56'
+    | '57'
+    | '58'
+    | '59'
+    | '60'
+    | '61'
+    | '62'
+    | '63'
+    | '64'
+export type GateRecord<T> = Record<GateNum, T>
 export interface Gate {
-    readonly num: number
+    readonly num: GateNum
     readonly ord: number
     readonly center: Center
-    readonly connected: Array<number>
+    readonly connected: Array<GateNum>
     readonly angle: Angle
 }
 
